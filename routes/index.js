1ad0044c22b6415f90ef29ce13faaea47cdfca8c
ABiteOfSorrow <laceican@naver.com>
2022-04-06 11:47:44 +0200

    connect with openWeather API for real time infomation

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 const cookieParser = require('cookie-parser');
 var express = require('express');
 var router = express.Router();
+var request = require('sync-request');
+/*
+router.get('/users', function (req, res, next) {
+  var requete = request("GET","https://jsonplaceholder.typicode.com/users");
+  var resultWS = JSON.parse(requete.body);
+  console.log(resultWS)
+  res.render('users', { users: resultWS});
+  }); */
+  //1ab23c3905561070e8f38cf42108df6d
 
-let cityList = [
-  {name: "Paris", weather: "nuageux", img: "/images/nuageux.png", highDegree: 28, lowDegree: 5.5},
-  {name: "London", weather: "nuageux", img: "/images/nuageux.png", highDegree: 28, lowDegree: 5.5},
-  {name: "Italie", weather: "nuageux", img: "/images/nuageux.png", highDegree: 28, lowDegree: 5.5},
-  {name: "Luxembourg", weather: "nuageux", img: "/images/nuageux.png", highDegree: 28, lowDegree: 5.5},
-]
+let cityList = []
+
+
 
 
 /* GET home page. */
@@ -16,11 +22,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/weather', function(req, res, next) {
+  const defaultLoaction = req.body.name ? req.body.name : "Paris"
+  let requete = request("GET","http://api.openweathermap.org/data/2.5/weather?q=" + defaultLoaction + "&units=metric&appid=1ab23c3905561070e8f38cf42108df6d");
+  let cityList = JSON.parse(requete.body)
   res.render('weather', { cityList: cityList });
 });
 
+
+
 router.post('/add-city', function (req, res, next) {
 
+  const setLoaction = req.body.newCity
+  var requete = request("GET","http://api.openweathermap.org/data/2.5/weather?q=" + setLoaction + "&units=metric&appid=1ab23c3905561070e8f38cf42108df6d");
+  var newCityList = JSON.parse(requete.body)
   let alreadyExist = false;
 
   for(let i=0; i<cityList.length; i++){
@@ -28,19 +42,22 @@ router.post('/add-city', function (req, res, next) {
       alreadyExist = true;
     }
   }
-
+ 
   if (alreadyExist == false){
   cityList.push({
   name: req.body.newCity,
-  weather: "nuageux",
-  img: "/images/nuageux.png",
-  highDegree: 28, 
-  lowDegree: 5.5
+  weather: newCityList.weather[0].description,
+  img: "http://openweathermap.org/img/w/"+ newCityList.weather[0].icon +".png",
+  temp_max: newCityList.main.temp_max, 
+  temp_min: newCityList.main.temp_min
 }
 )}
 
   res.render('weather', {cityList: cityList});
 });
+
+
+
 
 router.get('/delete-city', function(req, res, next) {
   cityList.splice(req.query.position, 1)
