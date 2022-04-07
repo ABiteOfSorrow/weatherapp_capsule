251216ae5d1c248a6a879ed9c0ef60f165eae915
ABiteOfSorrow <laceican@naver.com>
2022-04-07 09:54:02 +0200

    connect with mongoDB for save datas

---
+++ routes/bdd.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+var options = {
+    connectTimeoutMs: 5000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect('mongodb+srv://crelan:aa6690266@weatherapp.a2enh.mongodb.net/weatherapp?retryWrites=true&w=majority',
+options,function(error){
+    if(error){
+        console.log(error);
+    } else{
+        console.log("connection OK")
+    }}
+);
+
+var citySchema = mongoose.Schema({
+    name: String,
+    weather: String,
+    img: String,
+    temp_max: Number,
+    temp_min: Number
+})
+
+var cityModel = mongoose.model('cities', citySchema);
+
+module.exports = cityModel;
--- routes/index.js
+++ routes/index.js
@@ -2,67 +2,90 @@ const cookieParser = require('cookie-parser');
 var express = require('express');
 var router = express.Router();
 var request = require('sync-request');
-/*
-router.get('/users', function (req, res, next) {
-  var requete = request("GET","https://jsonplaceholder.typicode.com/users");
-  var resultWS = JSON.parse(requete.body);
-  console.log(resultWS)
-  res.render('users', { users: resultWS});
-  }); */
-  //1ab23c3905561070e8f38cf42108df6d
+var cityModel = require('./bdd')
+
 
 let cityList = []
 
 
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  res.render('login', {title: 'Express'});
 });
 
-router.get('/weather', function(req, res, next) {
-  const defaultLoaction = req.body.name ? req.body.name : "Paris"
-  let requete = request("GET","http://api.openweathermap.org/data/2.5/weather?q=" + defaultLoaction + "&units=metric&appid=1ab23c3905561070e8f38cf42108df6d");
-  let cityList = JSON.parse(requete.body)
-  res.render('weather', { cityList: cityList });
+/* GET weather page. */
+router.get('/weather', async function (req, res, next) {
+  cityList = await cityModel.find();
+  res.render('weather', {cityList: cityList});
 });
 
 
-
-router.post('/add-city', function (req, res, next) {
+/* Add city weather page. */
+router.post('/add_city', async function (req, res, next) {
 
   const setLoaction = req.body.newCity
-  var requete = request("GET","http://api.openweathermap.org/data/2.5/weather?q=" + setLoaction + "&units=metric&appid=1ab23c3905561070e8f38cf42108df6d");
+  var requete = request("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + setLoaction + "&units=metric&appid=1ab23c3905561070e8f38cf42108df6d");
   var newCityList = JSON.parse(requete.body)
   let alreadyExist = false;
 
-  for(let i=0; i<cityList.length; i++){
-    if(req.body.newCity.toLowerCase() == cityList[i].name.toLowerCase()){
+  for (let i = 0; i < cityList.length; i++) {
+    if (cityList[i].name.toLowerCase() == req.body.newCity.toLowerCase()) {
       alreadyExist = true;
     }
   }
- 
-  if (alreadyExist == false){
-  cityList.push({
-  name: req.body.newCity,
-  weather: newCityList.weather[0].description,
-  img: "http://openweathermap.org/img/w/"+ newCityList.weather[0].icon +".png",
-  temp_max: newCityList.main.temp_max, 
-  temp_min: newCityList.main.temp_min
-}
-)}
 
+let tempName = req.body.newCity.toLowerCase()
+let newCityName = tempName.charAt(0).toUpperCase() + tempName.slice(1)
+
+  if (alreadyExist == false) {
+    var newAddCity = new cityModel({
+      name: newCityName,
+      weather: newCityList.weather[0].description,
+      img: "http://openweathermap.org/img/w/" + newCityList.weather[0].icon + ".png",
+      temp_max: newCityList.main.temp_max,
+      temp_min: newCityList.main.temp_min
+    })
+    var citySaved = await newAddCity.save()
+    if (citySaved) {
+      console.log('Data input to BD Success!')
+    }
+  }
+  cityList = await cityModel.find();
   res.render('weather', {cityList: cityList});
 });
 
 
 
-
-router.get('/delete-city', function(req, res, next) {
-  cityList.splice(req.query.position, 1)
-  res.render('weather', { cityList: cityList });
+/* Delete city page. */
+router.get('/delete_city', async function (req, res, next) {
+  await cityModel.deleteMany({
+    name: req.query.deleteName
+  });
+  cityList = await cityModel.find();
+  res.render('weather', {cityList: cityList});
 });
 
 
-module.exports = router;
+
+/* Update city page. */
+router.get('/update_cities', async function (req, res, next) {
+
+  for (let i = 0; i < cityList.length; i++) {
+  const setLoaction = cityList[i].name
+  var requete = request("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + setLoaction + "&units=metric&appid=1ab23c3905561070e8f38cf42108df6d");
+  var newCityList = JSON.parse(requete.body)
+
+  await cityModel.updateOne(
+    {name: newCityList.name}, 
+    {name: newCityList.name,
+    weather: newCityList.weather[0].description,
+    img: "http://openweathermap.org/img/w/" + newCityList.weather[0].icon + ".png",
+    temp_max: newCityList.main.temp_max,
+    temp_min: newCityList.main.temp_min
+  });
+  console.log(newCityList.name + ' is updated')
+  }
+  cityList = await cityModel.find();
+  res.render('weather', {cityList: cityList});
+  });
+  module.exports = router;
